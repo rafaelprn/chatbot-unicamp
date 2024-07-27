b1bd359cc7203a64167f2e47600062c45dd19225
Rafael Perina <rafael.prn3@gmail.com>
2024-07-27 01:47:57 -0300

    feat: conversation history

--- chatbot.py
+++ chatbot.py
@@ -7,15 +7,23 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-def enviar_msg(mensagem):
-  chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user", "content": mensagem
-        }
-    ],
+def enviar_msg(mensagem, lista_mensagens=[]):
+  lista_mensagens.append(
+    {"role": "user", "content": mensagem}
+  )
+  resposta = client.chat.completions.create(
+    messages= lista_mensagens,
     model="llama3-8b-8192",
   )
-  return chat_completion.choices[0].message.content
+  return resposta.choices[0].message.content
 
-print(enviar_msg("Define the use of fast language models."))
+lista_mensagens = []
+while True: #TODO: arrumar erro na segunda pergunta (langchain?)
+  texto = input("Escreva sua mensagem: ")
+  
+  if texto == "sair":
+    break
+  else:
+    resposta = enviar_msg(texto, lista_mensagens)
+    lista_mensagens.append(resposta)
+    print("Chatbot: ", resposta)
