bdffc9e5ad057d9f2c8c21b46d87397bf96694b6
Rafael Perina <rafael.prn3@gmail.com>
2024-08-03 02:04:09 -0300

    feat: create vectorstore function

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,14 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain.llms.base import LLM
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+#from langchain_community.llms import GroqLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+#from langchain.chat_models import ChatGroq
 import pymupdf
 
 load_dotenv()
@@ -23,26 +31,6 @@ system_prompt = {
 # Inicializando o histórico do chat
 chat_history = [system_prompt]
 
-# class GroqLLM(LLM):
-#     def __init__(self, client):
-#         self.client = client
-
-#     def _call(self, prompt: str, stop=None):
-#         response = self.client.chat.completions.create(
-#             model="llama3-70b-8192",
-#             messages=[{"role": "user", "content": prompt}],
-#             max_tokens=200,
-#             temperature=1.2
-#         )
-#         return response.choices[0].message.content
-
-#     @property
-#     def _identifying_params(self):
-#         return {"model": "llama3-70b-8192"}
-
-#     @property
-#     def llm_type(self):
-#         return "groq"
 
 # Função para carregar e dividir o PDF
 def carregar_pdf(caminho_pdf):
@@ -50,12 +38,12 @@ def carregar_pdf(caminho_pdf):
 
     raw_text = extrair_texto(caminho_pdf)
     text_chunks = extrair_trechos_texto(raw_text) #retorna uma lista de strings
-    print(text_chunks)
+    print(len(text_chunks))
+    vectorstore = criar_vectorstore(text_chunks)
+    print(vectorstore)
+    conversation = create_conversation_chain(vectorstore)
 
     doc.close()
-    # splitter = RecursiveCharacterTextSplitter()
-    # docs_divididos = splitter.split_documents(doc)
-    # return docs_divididos
 
 def extrair_texto(caminho_pdf):
     doc = pymupdf.open(caminho_pdf)
@@ -74,6 +62,23 @@ def extrair_trechos_texto(raw_text):
     chunks = text_splitter.split_text(raw_text)
     return chunks
 
+def criar_vectorstore(chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=chunks, embeddings=embeddings)
+    return vectorstore
+
+def create_conversation_chain(vectorstore):
+    llm = ChatGroq(client)
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        vectorstore=vectorstore.as_retriever(),
+        memory=memory,
+    )
+    return conversation_chain
+    
+    
+    
 # Função para responder perguntas usando o LangChain
 # def responder_pergunta(pergunta, docs_divididos, llm):
 #     template = """
@@ -125,3 +130,24 @@ while True:
         # Imprimir a resposta no console
         print("Chatbot:", resposta_final)
 """
+
+# class GroqLLM(LLM):
+#     def __init__(self, client):
+#         self.client = client
+
+#     def _call(self, prompt: str, stop=None):
+#         response = self.client.chat.completions.create(
+#             model="llama3-70b-8192",
+#             messages=[{"role": "user", "content": prompt}],
+#             max_tokens=200,
+#             temperature=1.2
+#         )
+#         return response.choices[0].message.content
+
+#     @property
+#     def _identifying_params(self):
+#         return {"model": "llama3-70b-8192"}
+
+#     @property
+#     def llm_type(self):
+#         return "groq"
